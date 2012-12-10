92035cccc3e4ca4bf4792d1e3e8245c4c4b5e9df
iObitLXF <developer@RD-wentao-iMac.local>
2012-12-10 15:32:15 +0800

    Initial Commit

---
+++ FaceDetect/FaceDetectAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FaceDetectAppDelegate.h
+//  FaceDetect
+//
+//  Created by iObitLXF on 12/10/12.
+//  Copyright (c) 2012 iObitLXF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FaceDetectAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FaceDetect/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FaceDetect
+//
+//  Created by iObitLXF on 12/10/12.
+//  Copyright (c) 2012 iObitLXF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FaceDetectAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FaceDetectAppDelegate class]));
+    }
+}
